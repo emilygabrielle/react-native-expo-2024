738527fde167752afff28202a6324d0e5817b996
Emily Gabrielle Bonfim Dos Santos <161051107+emilygabrielle@users.noreply.github.com>
2024-08-15 18:46:14 +0000

    [feat] Configuracao-expo

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -23,8 +23,8 @@ const StackLayout = () => {
     }, [user]);
    return (
    <Stack>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(protected)" />
+        <Stack.Screen name="index" options={{headerShown: false}} />
+        <Stack.Screen name="(protected)" options={{headerShown: false}} />
     </Stack>
     );
 }
