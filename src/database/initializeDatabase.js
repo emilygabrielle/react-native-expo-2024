b531125ac3524fda25eb3ce8012be6bcb8af155b
Emily Gabrielle Bonfim Dos Santos <161051107+emilygabrielle@users.noreply.github.com>
2024-09-11 12:56:19 +0000

    [fix] Create Payments Datatable

--- src/database/initializeDatabase.js
+++ src/database/initializeDatabase.js
@@ -1,6 +1,9 @@
 export async function initializeDatabase(database) {
     try {
         await database.execAsync(`
+
+            DROP TABLE IF EXISTS payments;
+
             DROP TABLE IF EXISTS users;
             
             CREATE TABLE IF NOT EXISTS users (
@@ -13,6 +16,17 @@ export async function initializeDatabase(database) {
                 updated_at DATE
 
             );
+
+            CREATE TABLE IF NOT EXISTS payments (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              user_id INTEGER NOT NULL,
+              user_cadastro INTEGER NOT NULL,
+              valor_pago REAL NOT NULL,
+              data_pagamento DATE NOT NULL,
+              observacao TEXT,
+              created_at DATE DEFAULT CURRENT_TIMESTAMP,
+              updated_at DATE
+                );
 
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super', 'super@email.com', 'A123456a!', 'SUPER');
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Adm', 'adm@email.com', 'A123456a!', 'ADM');
